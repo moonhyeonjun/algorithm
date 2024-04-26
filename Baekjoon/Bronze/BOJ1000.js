e45ebe4bfe503b18a0506657dbfebfde77c134cf
moonhyeonjun <mhj5256@gmail.com>
2024-04-26 11:18:59 +0900

    Refactor code to use const and update variable names

--- Baekjoon/Bronze/BOJ1000.js
+++ Baekjoon/Bronze/BOJ1000.js
@@ -1,6 +1,8 @@
 /* 문제: A+B / 분류: 수학, 구현, 사칙연산 */
-var fs = require('fs');
-var input = fs.readFileSync('/dev/stdin').toString().split(' ');
-var a = parseInt(input[0]);
-var b = parseInt(input[1]);
-console.log(a + b);
+const fs = require('fs');
+const inputData = fs.readFileSync("/dev/stdin").toString().split(" ");
+
+const A = parseInt(inputData[0]);
+const B = parseInt(inputData[1]);
+
+console.log(A + B);
