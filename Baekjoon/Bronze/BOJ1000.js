f1a6d6cec6095f6829d6db4385fc85a6dda486d6
moonhyeonjun <mhj5256@gmail.com>
2024-04-29 08:14:23 +0900

    Remove unnecessary code files

--- Baekjoon/Bronze/BOJ1000.js
+++
@@ -1,8 +0,0 @@
-/* 문제: A+B / 분류: 수학, 구현, 사칙연산 */
-const fs = require('fs');
-const inputData = fs.readFileSync("/dev/stdin").toString().split(" ");
-
-const A = parseInt(inputData[0]);
-const B = parseInt(inputData[1]);
-
-console.log(A + B);
