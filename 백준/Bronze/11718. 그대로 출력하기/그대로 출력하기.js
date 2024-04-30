6f32bfaffae66a2ec4a0a6c23d0da06f9347ea08
Hyeonjun Moon <140958191+moonhyeonjun@users.noreply.github.com>
2024-04-30 10:31:07 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 136 ms, Memory: 9492 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.js
@@ -0,0 +1,6 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "run/input.txt";
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+input.forEach((str) => {
+    console.log(str);
+});
