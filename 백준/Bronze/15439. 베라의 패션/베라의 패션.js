9a927541edafee9d79c9a8f1d7a78b0c5ab60d18
Hyeonjun Moon <140958191+moonhyeonjun@users.noreply.github.com>
2024-05-02 09:22:20 +0900

    [Bronze IV] Title: 베라의 패션, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.js
@@ -0,0 +1,4 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "run/input.txt";
+const input = require('fs').readFileSync(filePath).toString().trim();
+
+console.log(input * (input - 1));
