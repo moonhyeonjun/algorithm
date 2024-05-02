a62c103cf42d284412de5a2372654f44229a4f17
Hyeonjun Moon <140958191+moonhyeonjun@users.noreply.github.com>
2024-05-02 13:35:57 +0900

    [Bronze IV] Title: 영수증, Time: 120 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.js
@@ -0,0 +1,12 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "run/input.txt";
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+const totalPrice = Number(input.shift());
+const list = input.slice(1);
+
+const realPrice = list.reduce((acc, cur) => {
+    const [price, count] = cur.split(' ').map(Number);
+    return acc + price * count;
+}, 0);
+
+totalPrice === realPrice ? console.log('Yes') : console.log('No');
