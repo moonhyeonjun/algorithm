34f67b9ed85a71152c9a820b89bae271c0e87fd1
Hyeonjun Moon <140958191+moonhyeonjun@users.noreply.github.com>
2024-05-02 16:10:15 +0900

    [Bronze V] Title: A+B - 2, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.js
@@ -0,0 +1,6 @@
+const filePath = process.platform === "linux" ? "dev/stdin" : "run/input.txt";
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n').map(Number);
+
+const [a, b] = input;
+
+console.log(a + b);
