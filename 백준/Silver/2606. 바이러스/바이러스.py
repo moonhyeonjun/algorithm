32b1e4526993db9f0e644d5990d12f80975a3b4f
Hyeonjun Moon <140958191+moonhyeonjun@users.noreply.github.com>
2024-08-05 12:48:10 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,22 @@
+def dfs(graph, start):
+    visited = []
+    stack = [start]
+
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.append(node)
+            stack.extend(graph[node])
+
+    return visited
+
+n = int(input())
+m = int(input())
+graph = {i: [] for i in range(1, n+1)}
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+print(len(dfs(graph, 1)) - 1)
