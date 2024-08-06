d38957442c07eaa16298127f15eba8c3d1bebc5f
Hyeonjun Moon <140958191+moonhyeonjun@users.noreply.github.com>
2024-08-06 10:04:35 +0900

    [Silver I] Title: 스타트와 링크, Time: 4884 ms, Memory: 56256 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,27 +1,20 @@
-N = int(input())
-board = [list(map(int,input().split())) for _ in range(N)]
-visited = [False for _ in range(N)]
-INF = 2147000000
-res = INF
+from itertools import combinations
 
-def DFS(L,idx):
-    global res
-    if L == N//2:
-        A = 0
-        B = 0
-        for i in range(N):
-            for j in range(N):
-                if visited[i] and visited[j]:
-                    A += board[i][j]
-                elif not visited[i] and not visited[j]:
-                    B +=board[i][j]
-        res = min(res, abs(A-B))
-        return
-    for i in range(idx,N):
-        if not visited[i]:
-            visited[i] = True
-            DFS(L+1,i+1)
-            visited[i] = False
-            
-DFS(0,0)
-print(res)
+N = int(input())
+S = [list(map(int, input().split())) for _ in range(N)]
+members = [i for i in range(N)]
+teams = list(combinations(members, N//2) )
+min_ability = 100*20*20
+
+for team in teams:
+    start_team = team
+    link_team = tuple(set(members) - set(team))
+    start_ability = 0
+    link_ability = 0
+    for i in range(N//2):
+        for j in range(i+1, N//2):
+            start_ability += S[start_team[i]][start_team[j]] + S[start_team[j]][start_team[i]]
+            link_ability += S[link_team[i]][link_team[j]] + S[link_team[j]][link_team[i]]
+    min_ability = min(min_ability, abs(start_ability - link_ability))
+    
+print(min_ability)
